277e55f9e0f118defb7215c349340a2741da296d
dev.uche <uchennahanson@gmail.com>
2025-08-15 22:28:58 +0100

    uche

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0"
-  }
+  build: {
+    input: {
+      main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
+      replay: resolve(fileURLToPath(new URL('.', import.meta.url)), 'replay.html'),
+      // Add more endpoints here as needed
+      // course: resolve(__dirname, 'course.html'),
+      // signup: resolve(__dirname, 'signup.html'),
+    },
+  },
 })
